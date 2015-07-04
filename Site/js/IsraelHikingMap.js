6a0f1f0b175762253678ae61f9c4f4c35e0d41ed
zstadler <zeev.stadler@gmail.com>
2015-07-04 14:04:39 +0300

    Allow "lon=" alternative for "lng=" URL parameter syntax
    Use "lon" as the longitude variable in the JS code

--- Site/js/IsraelHikingMap.js
+++ Site/js/IsraelHikingMap.js
@@ -3,10 +3,10 @@ $(document).ready(function($) {
 	// backwards compatibility - convert to new style
     var zoom = parseInt(getURLParameter('zoom'));
     var lat = parseFloat(getURLParameter('lat'));
-    var lng = parseFloat(getURLParameter('lng'));
-    if (zoom > 0 && lng > 0 && lat > 0)
+    var lon = parseFloat(getURLParameter('lon')) || parseFloat(getURLParameter('lng'));
+    if (zoom > 0 && lon > 0 && lat > 0)
     {
-		var href = window.location.pathname + "#" + zoom + "/" + lat + "/" + lng;
+		var href = window.location.pathname + "#" + zoom + "/" + lat + "/" + lon;
 		window.location.href = href;
     }
 });
